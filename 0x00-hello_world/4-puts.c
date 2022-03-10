1ccaf60fa737b06b0cec4f83d7733ae900471c5c
trillionclues <excel@gmail.com>
2022-03-10 15:16:33 -0800

    C program that prints a line text followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: 0
- */
-
+ *  * main - Entry point
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *    * Return: 0
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
