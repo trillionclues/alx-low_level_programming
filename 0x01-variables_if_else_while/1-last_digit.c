949d58c06edc2160abb6e7026e62c429244cb52f
trillionclues <excel@gmail.com>
2022-03-11 14:12:54 -0800

    C program to generate random numbers and use conditional statements on them

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - 1. The last digit
@@ -10,24 +10,26 @@
 
 int main(void)
 {
-	int n;
-	int a;
+	int n, lastd;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	a = n % 10;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastd = n % 10;
 
-	if(a>5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, a);
-	}
-	else if (a == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, a);
-	}
-	else if(a <6)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a)
-	}
+if (lastd > 5)
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+}
+
+else if (lastd == 0)
+{
+	printf("Last digit of %d is %d and is 0\n", n, lastd);
+}
+else if (lastd < 6 && lastd != 0)
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+}
+
 return (0);
+
 }
