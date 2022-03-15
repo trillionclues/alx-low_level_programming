f5aec298367dfd766fb571d804940c27c3be8795
trillionclues <excel@gmail.com>
2022-03-15 15:19:34 -0700

    function to print alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,17 @@
 
 /**main - Print alphabet in lowercase
  *
- * Description: function that prints the alphabet, in lowercase
+ * Description: function print_alphabet, in lowercase
+ * 
  * Returns: returns void (Success)
  */
 
 void print_alphabet(void)
 {
-	char alpha;
-	
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 _putchar (alpha);
 }
-
 _putchar ('\n');
 }
