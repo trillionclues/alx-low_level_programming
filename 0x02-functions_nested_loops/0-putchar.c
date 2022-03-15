09ee523004983720dde0a42df5bb9d35a3af130e
trillionclues <excel@gmail.com>
2022-03-15 09:10:02 -0700

    program to print file using  _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,15 @@
 
 int main(void)
 {
-	printf("_putchar");
-	putchar ('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
 	return (0);
 }
