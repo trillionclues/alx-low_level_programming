d690baac7787abda5b7e86602049c7917bd206f8
trillionclues <excel@gmail.com>
2022-03-15 15:23:04 -0700

    function to print alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 #include "main.h"
 
-/**main - Print alphabet in lowercase
+/**print_alphabet - Print alphabet in lowercase
  *
  * Description: function print_alphabet, in lowercase
  * 
- * Returns: returns void (Success)
+ * Return: void (Success)
  */
-
 void print_alphabet(void)
 {
 char alpha;
