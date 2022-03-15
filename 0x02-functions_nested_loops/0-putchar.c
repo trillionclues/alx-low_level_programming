20c6734f9961b239ad0bf9eafdc2dc92845d3281
trillionclues <excel@gmail.com>
2022-03-15 08:56:10 -0700

    program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints to string
+ * Description: prints _putchar followed by a line
+ * Return: 0
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("_putchar");
+	putchar ('\n');
+	return (0);
+}
