94b2b54f1181f4435e58c0896dc594a780d92987
trillionclues <excel@gmail.com>
2022-03-22 02:40:24 -0700

    function that swaps the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-/** 
- * swap_int - swaps the value of two integers
+/** swap_int - swaps the value of two integers
  * @a: Integer value
  * @b: integer value
  * Description: function swap_int to swap value of two integers
