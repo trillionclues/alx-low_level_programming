8982eb33dab89277702742517a875ac22a5577ae
trillionclues <excel@gmail.com>
2022-03-22 14:20:20 -0700

    function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy -  a function that copies the string
+ * @dest : character value
+ * @src  : character value
+ * Description - function that copies the string pointed to by src
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
