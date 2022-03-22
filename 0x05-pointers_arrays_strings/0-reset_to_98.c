e25f22f122266ca24ab3e9c86bffefa3e0417995
trillionclues <excel@gmail.com>
2022-03-22 02:24:51 -0700

    update parameter value using pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
-/** reset_to_98 - updates the value it points to 98
- * @n: integer value
+/**
+ * reset_to_98 - updates the value it points to 98
+ * @n: Integer value
+ *
+ * Description: using pointer to update parameter value
+ *
+ * Return: Always (void)
  */
 
 void reset_to_98(int *n)
