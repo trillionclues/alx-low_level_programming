d92017393637e78e3354eccfe4488979e8373330
trillionclues <excel@gmail.com>
2022-04-01 10:33:18 -0700

    prints number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,16 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
-  
-  if (argc == 1)
-  printf("%d\n", argc - 1);
-  else
-  {
-    for (i = 0; *argv; i++, argv++);
-    
-    printf("%d\n", i - 1);
-  }
+	int i;
 
-return (0);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++);
+
+		printf("%d\n", i - 1);
+	}
+
+	return (0);
 }
