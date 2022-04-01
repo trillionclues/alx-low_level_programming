4ed615b67f50532e6c291c4c9a5812d27d96e1d1
trillionclues <excel@gmail.com>
2022-04-01 10:53:25 -0700

    program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - program that adds positive numbes
+ * @argc: positive n arguments
+ * @argv: array args
+ * Return: sum
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int i, sum, num;
+	sum = 0;
+
+	if (argc < 3)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (argc-- && argc > 0)
+	{
+		for (i = 0; argv[argc][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+	return (sum);
+}
