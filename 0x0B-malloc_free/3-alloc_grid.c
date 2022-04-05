9665685f9da7c97ed3697af0bdb98a0680d6103a
trillionclues <excel@gmail.com>
2022-04-04 18:15:04 -0700

    returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,6 +12,7 @@
 int **alloc_grid(int width, int height)
 {
 	int **gridout;
+
 	int i,j;
 
 	if (width < 1 || height < 1)
@@ -20,7 +21,7 @@ int **alloc_grid(int width, int height)
 	gridout = malloc(height * sizeof(int *));
 	if (gridout == NULL)
 	{
-		free (gridout);
+		free(gridout);
 		return (NULL);
 	}
 
@@ -30,15 +31,15 @@ int **alloc_grid(int width, int height)
 		if (gridout[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
-				free (gridout[i]);
-			free (gridout);
+				free(gridout[i]);
+			free(gridout);
 			return (NULL);
 		}
 	}
 
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
-			gridout [i][j] = 0;
+			gridout[i][j] = 0;
 
 	return (gridout);
 }
