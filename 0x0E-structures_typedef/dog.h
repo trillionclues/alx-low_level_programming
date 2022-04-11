22897b3eace1efd92d5ee8886a212cfdfb4123f4
Trillionclues <exceln646@gmail.com>
2022-04-11 18:51:02 +0100

    struct with dog elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct animal with elements
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: This struct is for dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /*DOG_H*/
