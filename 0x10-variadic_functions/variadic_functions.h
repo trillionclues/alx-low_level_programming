a2eabb62fdf1ef4119145f9df6601efee08bd92e
Trillionclues <exceln646@gmail.com>
2022-04-14 12:40:00 +0100

    addition to header files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,30 +1,32 @@
 #ifndef VARIADIC_FUNCTIONSH
 #define VARIADIC_FUNCTIONSH
 #include <stdarg.h>
-
+#include <stdio.h>
 int _putchar(char c);
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 
-void print_int(va_list list);
-void print_float(va_list list);
-void print_char(va_list list);
-void print_str(va_list list);
+void print_int(va_list);
+void print_float(va_list);
+void print_char(va_list);
+void print_string(va_list);
 
 
 /**
- * struct printTypeStruct - struct definition of printTypeStruct function
- * @type: type of arg
- * @printer: function to print
+ * struct printTypeStruct - struct definition of entry sign
+ * @t: type of arg
+ * @f: function to print type of args
  */
 
-typedef struct printTypeStruct
+typedef struct format_t
 {
-	char *type;
-	void (*printer)(va_list);
-} printTypeStruct;
+	char *t;
+	void (*f)(va_list);
+} format_t;
 
 #endif /*VARIADIC_FUNCTIONSH*/
