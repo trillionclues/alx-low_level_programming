ba1e944baf6c7019ff1c31cde39f078703f705cd
Trillionclues <exceln646@gmail.com>
2022-04-21 20:18:58 +0100

    adds a new node at the beginning of linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,16 +1,18 @@
 #include "lists.h"
+
 /**
  * add_node - adds a new node at the beginning
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
+ * of a linked list
+ * @head: head of the linked list
+ * @str: string to store in the list
+ *
+ * Return: address of the head
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t nchar;
+	size_t nbr_node;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -18,10 +20,10 @@ list_t *add_node(list_t **head, const char *str)
 
 	new->str = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++)
+	for (nbr_node = 0; str[nbr_node]; nbr_node++)
 		;
 
-	new->len = nchar;
+	new->len = nbr_node;
 	new->next = *head;
 	*head = new;
 
