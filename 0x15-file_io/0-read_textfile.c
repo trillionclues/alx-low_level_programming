1eb3b0db8d35cefd296e4ac508880bb2035dc7ad
Trillionclues <exceln646@gmail.com>
2022-05-02 12:39:50 +0100

    reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,16 +2,14 @@
 
 /**
  * read_textfile - reads a text file and prints the letters
- * @filename: filename
- * @letters: numbers of letters printed
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: numbers of letters printed. It fails, returns 0
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-
 	ssize_t nrd, nwr;
 	char *buf;
 
