ac684ddf8068e85c20f00bcb3e4f34287e24e0cb
trillionclues <excel@gmail.com>
2022-06-09 17:46:29 -0700

    func to delete node at index of DLL

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,44 +9,41 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *pre, *headcopy = *head;
+	dlistint_t *h1;
+	dlistint_t *h2;
 	unsigned int i;
 
-	while (headcopy != NULL && headcopy->prev != NULL)
+	h1 = *head;
+
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		headcopy = headcopy->prev;
-		*head = (*head)->prev;
-	}
-	if (headcopy != NULL && index != 0)
-	{
-		for (i = 0; i < index && headcopy != NULL; i++)
+		if (i == index)
 		{
-			pre = headcopy;
-			headcopy = headcopy->next;
-		}
-		if (headcopy != NULL)
-		{
-			pre->next = headcopy->next;
-			if (pre->next != NULL)
-				headcopy->next->pre;
-			free(headcopy);
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
 			return (1);
 		}
-		return (-1);
-	}
-
-	if (headcopy != NULL && index == 0)
-	{
-		pre = headcopy->next;
-		if (pre == NULL)
-			*head = NULL;
-		else
-		{
-			pre->prev = NULL;
-			free(headcopy);
-			*head = pre;
-		}
-		return (1);
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
 
 	return (-1);
