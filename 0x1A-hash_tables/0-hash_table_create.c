a8840a29fb0a8ebe8c6e6ef63e134ab1866b08b0
trillionclues <excel@gmail.com>
2022-07-01 17:16:33 -0700

    function to create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int f;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	
+	for (f = 0; f < size; f++)
+		hash_table->array[f] = NULL;
+	return (hash_table);
+}
